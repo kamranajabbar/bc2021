c6652a8d00bf951fcec51341ce8bcac7bac45bc4
Kamran Jabbar <kamranajabbar@gmail.com>
2021-08-03 12:17:01 +0500

    class 04 completed

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,26 @@
-import { TodoItem } from "./todoItem";
+// import { TodoItem } from "./todoItem";
+
+// console.clear();
+
+// let item1: TodoItem = new TodoItem(1, "Order Mango", false);
+// let item2: TodoItem = new TodoItem(2, "Order Apple", false);
+
+// item1.printDetails();
+// item2.printDetails();
+
+
+import { ItemCollection } from "./itemCollection";
 
 console.clear();
 
-let item1: TodoItem = new TodoItem(1, "Order Mango", false);
-let item2: TodoItem = new TodoItem(2, "Order Apple", false);
+let col1: ItemCollection = new ItemCollection();
 
-item1.printDetails();
-item2.printDetails();
+col1.addTodo("Puchase Mango");
+col1.addTodo("Eat Biryani");
+col1.addTodo("Tikka");
+
+col1.taskDone(2);
+col1.taskDone(1);
+col1.taskDone(3);
+
+col1.printDetails();
